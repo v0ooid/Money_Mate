58bbed085f833088444a72ba107ffcecae739a84
ooizx-pm19@student.tarc.edu.my <ooizx-pm19@student.tarc.edu.my>
2023-11-01 12:25:33 +0800

    Bottom Navigation

--- app/src/main/java/my/edu/tarc/moneymate/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/moneymate/MainActivity.kt
@@ -2,10 +2,17 @@ package my.edu.tarc.moneymate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import my.edu.tarc.moneymate.databinding.ActivityAppLock6DigitBinding
+import my.edu.tarc.moneymate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView:BottomNavigationView
     }
 }
