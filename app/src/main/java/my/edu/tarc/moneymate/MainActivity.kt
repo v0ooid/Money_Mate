f0fcf3665a1b389dc893ac4cfcecfecdbda2aefa
ooizx-pm19@student.tarc.edu.my <ooizx-pm19@student.tarc.edu.my>
2023-11-01 15:48:28 +0800

    Bottom Navigation

--- app/src/main/java/my/edu/tarc/moneymate/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/moneymate/MainActivity.kt
@@ -2,6 +2,10 @@ package my.edu.tarc.moneymate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import my.edu.tarc.moneymate.databinding.ActivityAppLock6DigitBinding
 import my.edu.tarc.moneymate.databinding.ActivityMainBinding
@@ -13,6 +17,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView:BottomNavigationView
+        val navView:BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_container)'
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home
+            )
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
